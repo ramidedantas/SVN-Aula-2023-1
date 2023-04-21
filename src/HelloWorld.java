ad5a5a054114b79aab507877e6724b56e5c6c681
Karine Santos <89669182+jkss13@users.noreply.github.com>
2023-04-21 14:52:58 -0300

    adding my name

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,8 +4,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!");
 		System.out.println("Copia 1 & 2");
-		System.out.println("Nicolas Araújo");
+		System.out.println("Nicolas Araï¿½jo");
 		System.out.println("Lucas de Oliveira");
+		System.out.println("Karine Santos");
 	}
 	
 }
